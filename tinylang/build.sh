7c1bdb5b05cb11f7aa27be6c417f7587c3ff08d1
Velaciela <339256209@qq.com>
2022-03-28 15:32:59 +0800

    setup tinylang

---
+++ tinylang/build.sh
@@ -0,0 +1,16 @@
+rm -rf build
+rm -rf output
+
+cmake -S ../tinylang/ \
+      -B build \
+      -G Ninja  -DCMAKE_CXX_STANDARD=14 \
+                -DCMAKE_BUILD_TYPE=Release \
+                -DLLVM_DIR=/usr/local/lib/cmake/llvm \
+                -DCMAKE_INSTALL_PREFIX=./output
+
+cd build
+ninja
+ninja install
+cd ..
+
+
