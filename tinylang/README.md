3ff980f71abbf8e432703a531ad9a503e96d50b5
Velaciela <339256209@qq.com>
2022-03-29 16:03:19 +0800

    [tinylang] add comments for cmake system

--- tinylang/README.md
+++ tinylang/README.md
@@ -1,5 +1,5 @@
 # tinylang
 
-Follow the Tinylang project in [Learn-LLVM-12](https://github.com/PacktPublishing/Learn-LLVM-12) to learn about the basic usage of modules in LLVM.
+Follow the Tinylang project in [Learn-LLVM-12](https://github.com/PacktPublishing/Learn-LLVM-12) to learn about the basic usage of LLVM modules.
 
 Make sure [llvm-project](https://github.com/llvm/llvm-project) has been installed correctly by [../build_llvm_project.sh](../build_llvm_project.sh)
--- tinylang/cmake/modules/AddTinylang.cmake
+++ tinylang/cmake/modules/AddTinylang.cmake
@@ -1,13 +1,20 @@
-macro(add_tinylang_subdirectory name)
+macro(add_tinylang_subdirectory name) # like function
+  message(STATUS ">>> add_tinylang_subdirectory = ${name}") # 2\ add_tinylang_subdirectory = driver
+  message(STATUS ">>>    ARGN = ${ARGN}")
   add_llvm_subdirectory(TINYLANG TOOL ${name})
+  # function(add_llvm_subdirectory project type name)
+  # defined in llvm-project/llvm/cmake/modules/AddLLVM.cmake
 endmacro()
 
 macro(add_tinylang_library name)
+  message(STATUS ">>> add_tinylang_library = ${name}") # 1\ add_tinylang_library = tinylangBasic
+  message(STATUS ">>>    ARGN = ${ARGN}") # ARGN = version.cpp
   if(BUILD_SHARED_LIBS)
     set(LIBTYPE SHARED)
   else()
     set(LIBTYPE STATIC)
   endif()
+  # function(llvm_add_library name)
   llvm_add_library(${name} ${LIBTYPE} ${ARGN})
   if(TARGET ${name})
     target_link_libraries(${name} INTERFACE ${LLVM_COMMON_LIBS})
@@ -22,10 +29,14 @@ macro(add_tinylang_library name)
 endmacro()
 
 macro(add_tinylang_executable name)
+  message(STATUS ">>> add_tinylang_executable = ${name}") # 4\ add_tinylang_executable = tinylang
+  message(STATUS ">>>    ARGN = ${ARGN}") # ARGN = driver.cpp
   add_llvm_executable(${name} ${ARGN} )
 endmacro()
 
 macro(add_tinylang_tool name)
+  message(STATUS ">>> add_tinylang_tool = ${name}") # 3\ add_tinylang_tool = tinylang
+  message(STATUS ">>>    ARGN = ${ARGN}") # ARGN = driver.cpp
   add_tinylang_executable(${name} ${ARGN})
   install(TARGETS ${name}
     RUNTIME DESTINATION bin
