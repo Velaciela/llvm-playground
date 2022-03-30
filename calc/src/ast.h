af000276077853f2a473a7d04f6a2aa78324aca6
Velaciela <339256209@qq.com>
2022-03-30 16:37:10 +0800

    [calc] parser_2

---
+++ calc/src/ast.h
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/StringRef.h"
+
+class AST;
+class Expr;
+class Factor;
+class BinaryOp;
+class WithDecl;
+
+class ASTVisitor {
+// The visitor pattern needs to know each class it must visit. 
+// Because each class also refers to the visitor, 
+//     we declare all the classes at the top of the file. 
+// Note that the visit() methods for AST and Expr have a default implementation, which does nothing.
+public:
+    virtual void visit(AST &) {};
+    virtual void visit(Expr &) {};
+    virtual void visit(Factor &) = 0;
+    virtual void visit(BinaryOp &) = 0;
+    virtual void visit(WithDecl &) = 0;
+};
+
+// The AST class is the root of the hierarchy
+class AST {
+public:
+    virtual ~AST() {}
+    virtual void accept(ASTVisitor &v) = 0;
+};
+
+// Expr is the root for the AST classes related to expressions
+class Expr : public AST {
+public:
+    Expr() {}
+}
+
+// The Factor class stores a number or the name of a variable
+class Factor : public Expr {
+public:
+    enum ValueKind { Ident, Number };
+
+private:
+    ValueKind kind_;
+    llvm::StringRef val_;
+
+public:
+    Factor(ValueKind kind, llvm::StringRef val) : kind_(kind), val_(val) {}
+    ValueKind getKind() { return kind_; }
+    llvm::StringRef getVal() { return val_; }
+    virtual void accept(ASTVisitor &v) override {
+        v.visit(*this);
+    }
+};
+
+// In this example, numbers and variables are treated almost identically, 
+// so we create only one AST node class to represent them. 
+// The 'kind_' member tells us which of both cases the instances represent. 
+// In more complex languages, you usually want to have different AST classes, 
+//     such as a NumberLiteral class for numbers 
+//     and a VariableAccess class for a reference to a variable.
+
+
+// The BinaryOp class holds the data that's needed to evaluate an expression
+// The BinaryOp class makes no distinction between multiplicative and additive operators. 
+// The precedence of the operators is implicitly available in the tree structure.
+class BinaryOp : public Expr {
+
+public:
+    enum Operator { Plus, Minus, Mul, Div };
+
+private:
+    Expr *left_;
+    Expr *right_;
+    Operator op_;
+
+public:
+    BinaryOp(Operator op, Expr *l, Expr *r)
+        : op_(op), left_(l), right_(r) {}
+    Expr *getleft() { return left_; }
+    Expr *getRight() { return right_; }
+    Operator getOperator() { return op_; }
+    virtual void accept(ASTVisitor &v) override {
+        v.visit(*this);
+    }
+};
+
+
+// WithDecl stores the declared variables and the expression
+class WithDecl : public AST {
+    using VarVector = llvm::SmallVector<llvm::StringRef, 8>;
+    VarVector vars_;
+    Expr *e_;
+
+public:
+    WithDecl(llvm::SmallVector<llvm::StringRef,8> vars, Expr *e)
+        : vars_(vars), e_(e) {}
+    VarVector::const_iterator begin() {return vars_.begin(); }
+    VarVector::const_iterator end() { return vars_.end(); }
+    Expr *getExpr() { return e_; }
+    virtual void accept(ASTVisitor &v) override {
+        v.visit(*this);
+    }
+};
+
+
+// The AST is constructed during parsing.
+
+// The semantic analysis checks that the tree adheres to the meaning of the language 
+//     (for example, that used variables are declared) 
+//     and possibly augments the tree. 
+
+// After that, the tree is used for code generation.
+
--- calc/src/parser.cpp
+++ calc/src/parser.cpp
@@ -45,10 +45,78 @@ AST *Parser::parseCalc() {
         return new WithDecl(vars, e);
 
 // Detecting a syntax error is easy but recovering from it is surprisingly complicated
-// a simple approach called panic mode must be used
+// a simple approach called panic mode must be used.
+//     In panic mode, tokens are deleted from the token stream 
+//         until one is found that the parser can use to continue its work. 
+//     Most programming languages have symbols that denote an end; 
+//         for example, in C++, we can use ; (end of a statement) or } (end of a block). 
+//         Such tokens are good candidates to look for.
 _error:
     while (!tok_is(Token::eoi))
         advance();
     return nullptr;
 }
 
+
+// The other parsing methods are constructed in a similar fashion. 
+// parseExpr() is the translation of the rule for expr
+//     expr : term (( "+" | "-" ) term)* ;
+Expr *Parser::parseExpr() {
+    Expr *left = parseTerm();
+    while (tok_.isOneOf(Token::plus, Token::minus)) {
+        // repeated group inside the rule is translated into a while loop.
+        // note how the use of the isOneOf() method simplifies the check for several tokens.
+        BinaryOp::operator op = tok_.is(Token::plus) ?
+                                BinaryOp::Plus : BinaryOp::Minus;
+        advance();
+        Expr *right = parseTerm();
+        left = new BinaryOp(op, left, right);
+    }
+    return left;
+}
+
+
+// term : factor (( "*" | "/") factor)* ;
+Expr *Parser::parseTerm() {
+    Expr *left = parseFactor();
+    while (tok_.isOneOf(Token::star, Token::slash)) {
+        BinaryOp::Operator op = tok_.is(Token::star) ?
+                                BinaryOp::Mul : BinaryOp::Div;
+        advance();
+        Expr *right = parseFactor();
+        left = new BinaryOp(op, left, right);
+    }
+    return left;
+
+
+// factor : ident | number | "(" expr ")" ;
+Expr *Parser::parseFactor() {
+    Expr *res = nullptr;
+    switch (tok_.getKind()) {
+        case Token::number:
+            res = new Factor(Factor::Number, Tok.getText());
+            advance(); break;
+        case Token::ident:
+            res = new Factor(Factor::Ident, Tok.getText());
+            advance(); break;
+        case Token::l_paren:
+            advance(); 
+            res = parseExpr();
+            if (!consume(Token::r_paren)) break;
+        default:
+            if (!res) error();
+            while (!tok_.isOneOf(Token::r_paren, 
+                                 Token::star,
+                                 Token::plus,
+                                 Token::minus,
+                                 Token::slash,
+                                 Token::eoi))
+                advance();
+    }
+    return res;
+}
+
+
+// As soon as you have memorized the used patterns, 
+// it is almost tedious to code the parser based on the grammar rules. 
+// This type of parser is called a recursive descent parser.
